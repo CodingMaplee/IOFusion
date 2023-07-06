921f6c1f728bbded92b7fa6d8294cd48b7333109
Zhu_Haowei <105534052+CodingMaplee@users.noreply.github.com>
2023-07-06 17:54:02 +0800

    Add files via upload

---
+++ include/GlobalDefines.h
@@ -0,0 +1,36 @@
+
+#pragma once
+#ifndef _GLOBAL_DEFINES_
+#define _GLOBAL_DEFINES_
+
+#define MINF __int_as_float(0xff800000)
+
+#define MAX_MATCHES_PER_IMAGE_PAIR_RAW 128
+#define MAX_MATCHES_PER_IMAGE_PAIR_FILTERED 25
+
+/////////////////dynamic//////////////////
+#define NODE_KNN 8
+#define VERTEX_KNN 4
+#define VERTICES_RADIUS 0.01f
+#define NODES_RADIUS 0.08f
+#define CONSISTENCY_THRESHOLD 6.0f
+#define VERTEX_DOWN_SAMPLE 4
+#define CONSISTENCY_MIN_NUM 50
+#define CONCAVITY_MIN 0.02
+#define CONCAVITY_MAX 0.07
+#define IS_FILTER_SDF_RESIDUAL false
+#define EXTEND_KEYFRAMES_DYNAMIC_REGION true
+#define OPTIMIZED_TIMES 20
+
+#define PERSON_CAT 2
+
+#define DYNAMIC_BOX_DILATATION 40
+#define DYNAMIC_BOX_ERODE 10
+#define FRAME_WIDTH 640
+#define FRAME_HEIGHT 480
+#define RECONSTRUCT_GRAY true
+
+
+#define USE_LIE_SPACE
+
+#endif //_GLOBAL_DEFINES_
